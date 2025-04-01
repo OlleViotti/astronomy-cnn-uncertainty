c32dde6529c358d1b099e751ae8bc86fb947b8ec
OlleViotti <159911573+OlleViotti@users.noreply.github.com>
2025-04-01 15:00:19 +0200

    Add files via upload
    Initial commit

---
+++ seaborn_config.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme(style="whitegrid")
+
+plt.rcParams.update(
+    {
+        "axes.titlesize": 16,  # Title size
+        "axes.labelsize": 14,  # X and Y label size
+        "xtick.labelsize": 12,  # X tick label size
+        "ytick.labelsize": 12,  # Y tick label size
+        "legend.fontsize": 12,  # Legend font size
+        "figure.titlesize": 18,  # Overall figure title size
+        "font.size": 12,  # Base font size
+    }
+)
